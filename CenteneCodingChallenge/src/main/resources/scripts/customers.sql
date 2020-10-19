761a78365ec9fce380d2f928105ee14fc5e7b48c
Haley Howell <haleyhowell@Haleys-MacBook-Air-2.local>
2020-10-18 19:40:27 -0700

    Adding sql scripts

---
+++ CenteneCodingChallenge/src/main/resources/scripts/customers.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `enrollee` VALUES (1,'08/03/1995','Bob','5201111111',0),(3,'01/15/1963','Jimmy','5202222222',1),(4,'08/22/2002','Claire','5203333333',0);
+INSERT INTO `dependents` VALUES (1,1,1),(3,2,1),(5,3,4);
+INSERT INTO `dependent` VALUES (1,'03/08/2002','Prince'),(2,'01/19/2002','Duke'),(3,'04/23/1992','Jax');
